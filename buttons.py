eeda6b4cd95e0d84fef9bc8f746653260f2e1b3c
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-06-03 12:27:07 +0300

    template Class for button creation

---
+++ buttons.py
@@ -0,0 +1,13 @@
+import customtkinter as ctk
+
+class Buttons:
+    def __init__(self, master, text, command, row, column):
+        self.button = ctk.CTkButton(master, 
+                                    width=55, 
+                                    height=35,
+                                    text=text,
+                                    command=command,
+                                    font=('Arial', 25),
+                                    corner_radius=5
+                                    )
+        self.button.grid(row=row, column=column, padx=5, pady=0.5)
