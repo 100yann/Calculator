db8c4d28c295eab5a8f1103517e983c7adc75b0d
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-06-03 11:56:04 +0300

    make a switch that toggles dark and light mode

--- main.py
+++ main.py
@@ -8,7 +8,24 @@ app.resizable(False, False)
 app.title('Calculator')
 
 
+# make it dark or light mode
+def appearanceMode():
+    if switch_var.get() == 'on':
+        ctk.set_appearance_mode('dark')
+    else:
+        ctk.set_appearance_mode('light')
 
+
+# dark or light mode switch
+switch_var = ctk.StringVar(value='on')
+dark_or_light = ctk.CTkSwitch(app, 
+                              text='', 
+                              command=appearanceMode, 
+                              variable=switch_var,
+                              onvalue='on',
+                              offvalue='off',
+                              )
+dark_or_light.grid(row=0, column=1)
 
 
 
