bf96f3a7f44bfce219515eb8a95c3e39fb603b36
100yann <stoyan.d.kolev@gmail.com>
2023-06-04 13:14:34 +0300

    stylistic changes

--- buttons.py
+++ buttons.py
@@ -3,11 +3,15 @@ import customtkinter as ctk
 class Buttons:
     def __init__(self, master, text, command, row, column):
         self.button = ctk.CTkButton(master, 
-                                    width=55, 
-                                    height=35,
+                                    width=85, 
+                                    height=75,
                                     text=text,
                                     command=command,
                                     font=('Arial', 25),
-                                    corner_radius=5
+                                    corner_radius=5,
+                                    fg_color='gray20',
+                                    text_color='gray90',
+                                    hover_color='gray30',
+                                    bg_color='transparent'
                                     )
-        self.button.grid(row=row, column=column, padx=0, pady=0)
+        self.button.grid(row=row, column=column, padx=1, pady=1)
